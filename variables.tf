f54e9285bda214cc77b9a18585e4023675a4c0df
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-03-03 09:09:46 -0300

    feat: add variables

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "envVersion" {
+  default = "v1"
+}
+
+variable "product" {
+  default = "rocketseat"
+}
+
+variable "project" {
+  default = "travels-backend"
+}
+
+variable "role" {
+  default = "db"
+}
+
+variable "owner" {
+  default = "squad-rocketseat"
+}
+
+variable "createdBy" {
+  default = "devops-tools"
+}
