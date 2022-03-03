7d3bd02cb5fd09e293a19af850469cb4a52ae75d
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-03-03 20:57:25 -0300

    fix: bucket name

--- devops/iac/deploy/deploy.sh
+++ devops/iac/deploy/deploy.sh
@@ -10,7 +10,7 @@ check_sucessful(){
 init(){
   terraform init \
     -backend=true \
-    -backend-config="bucket=rocketseat-${REMOTE_STATE_BUCKET}-${REMOTE_STATE_AWS_REGION}-${ENV}-${ENV_VERSION}" \
+    -backend-config="bucket=${REMOTE_STATE_BUCKET}-${REMOTE_STATE_AWS_REGION}-${ENV}-${ENV_VERSION}" \
     -backend-config="key=${REMOTE_STATE_FILE}" \
     -backend-config="region=${REMOTE_STATE_AWS_REGION}" 
 }
