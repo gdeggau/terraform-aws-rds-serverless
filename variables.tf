c96fce80c5f34907224f4bbc28bd3f844690e403
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-03-03 09:50:19 -0300

    chore: add rds variables

--- variables.tf
+++ variables.tf
@@ -29,3 +29,56 @@ variable "owner" {
 variable "createdBy" {
   default = "devops-tools"
 }
+
+variable "engine" {
+  default = "aurora-postgresql"
+}
+
+variable "engineVersion" {
+  default = "10.7"
+}
+variable "engineMode" {
+  default = "serverless"
+}
+
+variable "databaseName" {
+  default = "travels"
+}
+
+variable "databaseUsername" {
+  default = "travels"
+}
+
+variable "backupRetentionPeriod" {
+  default = "5"
+}
+
+variable "storageEncrypted" {
+  default = "true"
+}
+
+variable "deletionProtection" {
+  default = "true"
+}
+# SHOULD BE false IN A PRODUCTION ENVIRONMENT
+variable "skipFinalSnapshot" {
+  default = "true"
+}
+variable "copyTagsToSnapshot" {
+  default = "true"
+}
+variable "autoPause" {
+  default = "true"
+}
+variable "secondsUntilAutoPause" {
+  default = "300"
+}
+variable "minCapacity" {
+  default = "2"
+}
+variable "maxCapacity" {
+  default = "8"
+}
+variable "timeoutAction" {
+  default = "ForceApplyCapacityChanges"
+}
