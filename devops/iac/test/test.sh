abfd8ec355a2d997f44653ff4ba5a5ada803f02f
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-03-03 21:09:44 -0300

    chore: hardcode bucket in test script

--- devops/iac/test/test.sh
+++ devops/iac/test/test.sh
@@ -10,7 +10,7 @@ check_sucessful(){
 init(){
   terraform init \
     -backend=true \
-    -backend-config="bucket=${REMOTE_STATE_BUCKET}-${REMOTE_STATE_AWS_REGION}-${ENV}-${ENV_VERSION}" \
+    -backend-config="bucket=rocketseat-terraform-remote-state-us-east-1-dev-v1" \
     -backend-config="key=${REMOTE_STATE_FILE}" \
     -backend-config="region=${REMOTE_STATE_AWS_REGION}" 
 }
